5fe8b6db0f8b8f505617640c4ea1b9edc56fee55
XianZheng Lin <2520505328@stu.gdpu.edu.cn>
2026-09-24 16:58:19 +0800

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using PlayerClassLib;
+
+namespace Test
+{
+    class Person
+    {
+        public int money;
+        public bool sex;
+        public Person() { 
+            money = 0;
+            sex = false;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] arr = new char[4] { 'a', 'b', 'c', 'c' };
+            string[] arr2 = new string[] { "abcc" };
+            Person p = new Person() { money = 100, sex = true };
+            Console.WriteLine(p.money);
+        }
+    }
+}
